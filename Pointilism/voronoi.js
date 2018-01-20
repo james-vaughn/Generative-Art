72290c0553d519da3a4e4fa512179a0ba2a174ff
James <james.thomas.vaughn@gmail.com>
2018-01-19 23:49:10 -0500

    Fleshing out smapling

--- Pointilism/voronoi.js
+++ Pointilism/voronoi.js
@@ -1,34 +1,40 @@
 const fs = require("fs");
-const canvas = require("canvas");
+const Canvas = require("canvas");
 const {Point} = require("../lib/point");
 
-const context = canvas.getContext("2d");
-const pngStream = canvas.pngStream();
-const outputImageFile = fs.createWriteStream(__dirname + "/../output/pointilism");
+const Image = Canvas.Image;
 
-pngStream.on("data", function (chunk) {
-    outputImageFile.write(chunk);
-});
+const outputImageFile = fs.createWriteStream(__dirname + "/../output/pointilism.png");
 
-fs.readFile(_dirname + "../input/pointilism.png", function (err, input_img) {
+fs.readFile(__dirname + "/../input/pointilism.png", function (err, input_img) {
    if(err) throw err;
 
    const img = new Image();
    img.src = input_img;
+
+   const canvas = new Canvas(img.width, img.height);
+   const context = canvas.getContext("2d");
+
    context.drawImage(img, 0, 0, img.width, img.height);
 
-   points = samplePoints(context);
+   points = samplePoints(img.width, img.height);
+   console.log(points);
    // colorGrid = voronoi(context, points);
    // drawImage(colorGrid);
+
+   const pngStream = canvas.pngStream();
+   pngStream.on("data", function (chunk) {
+       outputImageFile.write(chunk);
+   });
 });
 
 //returns an array of points to sample
-function samplePoints(context) {
+function samplePoints(width, height) {
     const num_random_points_per_sample = 20;
-    const poisson_inner_radius = 30;
+    const poisson_inner_radius = 50;
     const poisson_outer_radius = 2 * poisson_inner_radius;
 
-    const activePoints = [Point.random(context.width, context.height)];
+    const activePoints = [Point.random(width, height)];
     const samplePoints = [];
 
     while(activePoints.length > 0) {
@@ -41,27 +47,30 @@ function samplePoints(context) {
         // generate possible sample points
         for(let i = 0; i < num_random_points_per_sample; i++) {
             // TODO fix this so it properly calcs values withing the Poisson disc
-            // TODO Also, dont let points go outside grid
-            const delta_x = Math.floor(Math.random() * (poisson_outer_radius - poisson_inner_radius) + poisson_inner_radius);
-            const delta_y = Math.floor(Math.random() * (poisson_outer_radius - poisson_inner_radius) + poisson_inner_radius);
-            possible_sample_points.push(new Point(samplePoint.x - delta_x, samplePoint.y - delta_y));
+            const delta_x = Math.floor(2 * (Math.random() - .5) * (poisson_outer_radius - poisson_inner_radius) + poisson_inner_radius);
+            const delta_y = Math.floor(2 * (Math.random() - .5) * (poisson_outer_radius - poisson_inner_radius) + poisson_inner_radius);
+            const pt = new Point(samplePoint.x + delta_x, samplePoint.y + delta_y);
+
+            if(pt.x >= 0 && pt.x < width && pt.y >= 0 && pt.y < height) {
+                possible_sample_points.push(pt);
+            }
         }
 
         // remove bad points
-        possible_sample_points.filter(function (point) {
-           return activePoints.some((activePoint) => activePoint.withinDistance(point, poisson_inner_radius));
+        const points = possible_sample_points.filter(function (point) {
+           return activePoints.some((activePoint) => activePoint.withinDistance(point, poisson_inner_radius)) === false &&
+                  samplePoints.some((samplePoint) => samplePoint.withinDistance(point, poisson_inner_radius)) === false;
         });
 
         // If no points are left, mark the point as an inactive sample point
-        if(possible_sample_points.length === 0) {
+        if(points.length === 0) {
             samplePoints.push(samplePoint);
             continue;
         }
 
-        // add the remaining points to the active points list
-        for(const point of possible_sample_points) {
-            activePoints.push(point);
-        }
+        // add one of the possible points to the points list
+        activePoints.push(points[0]);
+        activePoints.push(samplePoint);
     }
 
     return samplePoints;
