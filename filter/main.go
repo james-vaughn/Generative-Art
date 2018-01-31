d9156facac0a9c1c4ad036e12faa381ddc377e2a
James <james.thomas.vaughn@gmail.com>
2018-01-31 12:07:17 -0500

    Have simplex noise bg working

--- filter/main.go
+++ filter/main.go
@@ -29,18 +29,36 @@ func main() {
 	img, _, _ := image.Decode(imageReader)
 	context.DrawImage(img, 0, 0)
 
-	context.SavePNG("../output/filter.png")
+	context.SavePNG("../output/filter_with_texture.png")
 }
 
 // Draws the background for the image to go over
 func drawBackground(context *gg.Context) {
+	drawSimplexNoiseBackground(context)
+}
+
+func drawGradientBackground (context *gg.Context) {
 	gradient := gg.NewLinearGradient(0, 0, 1000, 1000)
 	gradient.AddColorStop(0, color.White)
 	gradient.AddColorStop(1, color.Black)
 	context.SetFillStyle(gradient)
 	context.DrawRectangle(0, 0, WIDTH, HEIGHT)
 	context.Fill()
-	
-	noiseGen := noise.New()
-	_ = noiseGen
+}
+
+func drawSimplexNoiseBackground (context *gg.Context) {
+	noiseGen := noise.NewWithSeed(0) // TODO use random seed
+	scale := 10
+
+	for y := 0; y < HEIGHT; y++ {
+		for x := 0; x < WIDTH; x++ {
+			x_val := float64(scale * x)/WIDTH
+			y_val := float64(scale * y)/HEIGHT
+
+			n := (noiseGen.Eval2(x_val, y_val) + 1) * (255 / 2)
+			noiseVal := uint8(n)
+			context.SetColor(color.RGBA{ noiseVal, noiseVal, noiseVal, 255})
+			context.SetPixel(x, y)
+		}
+	}
 }
