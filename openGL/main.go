9b0110408cbae7302188421190fb5067592504c4
James <james.thomas.vaughn@gmail.com>
2018-02-06 21:42:09 -0500

    Fixed context issue

--- openGL/main.go
+++ openGL/main.go
@@ -1,14 +1,15 @@
 package main
 
 import (
-	"runtime"
 	"log"
+	"runtime"
+
 	"github.com/go-gl/gl/v4.5-core/gl"
 	"github.com/go-gl/glfw/v3.2/glfw"
 )
 
 const (
-	WIDTH int = 1000
+	WIDTH  int = 1000
 	HEIGHT int = 1000
 )
 
@@ -24,29 +25,30 @@ func main() {
 
 	window, err := createWindow()
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
 	defer window.Destroy()
 
 	// Initialize Glow
 	if err := gl.Init(); err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 
-	program, err := NewProgram("", "")
-	if err != nil {
-		panic(err)
-	}
-
-	defer program.Delete()
+	//program, err := NewProgram("vert.shader", "frag.shader")
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//
+	//defer program.Delete()
 
 }
-
 
 func createWindow() (*glfw.Window, error) {
 	glfw.WindowHint(glfw.Resizable, glfw.False)
 	glfw.WindowHint(glfw.OpenGLProfile, glfw.OpenGLCoreProfile)
+	glfw.WindowHint(glfw.ContextVersionMajor, 4)
+	glfw.WindowHint(glfw.ContextVersionMinor, 5)
 	glfw.WindowHint(glfw.OpenGLForwardCompatible, glfw.True)
 	window, err := glfw.CreateWindow(WIDTH, HEIGHT, "Hail Mary", nil, nil)
 	if err != nil {
