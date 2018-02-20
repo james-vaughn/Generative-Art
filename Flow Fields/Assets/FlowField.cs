4f5013c4a6f364f4a4ad7e9d5b5ed17d64bd925e
James <james.thomas.vaughn@gmail.com>
2018-02-20 14:51:28 -0500

    Have basic idea working

---
+++ Flow Fields/Assets/FlowField.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//https://docs.unity3d.com/ScriptReference/ParticleSystem.html
+//https://docs.unity3d.com/ScriptReference/ParticleSystem.TrailModule.html
+public class FlowField : MonoBehaviour
+{
+	private ParticleSystem ps;
+	private Gradient gradient = new Gradient();
+	public bool swapColors = true;
+
+	void Start()
+	{
+		gradient.SetKeys(
+			new GradientColorKey[] { new GradientColorKey(Color.blue, 0.0f), new GradientColorKey(Color.green, 1.0f) },
+			new GradientAlphaKey[] { new GradientAlphaKey(1.0f, 0.0f), new GradientAlphaKey(1.0f, 1.0f) }
+		);
+
+		ps = GetComponent<ParticleSystem>();
+
+		var trails = ps.trails;
+		trails.enabled = true;
+
+		var psr = GetComponent<ParticleSystemRenderer>();
+		psr.trailMaterial = new Material(Shader.Find("Sprites/Default"));
+		trails.colorOverTrail = gradient;
+	}
+}
---
+++ Flow Fields/Assets/ScreenCapure.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//https://docs.unity3d.com/ScriptReference/Application.CaptureScreenshot.html
+public class ScreenCapure : MonoBehaviour {
+	void OnMouseDown() {
+		ScreenCapture.CaptureScreenshot("flowFields.png");
+		Debug.Log ("Captured");
+	}
+}
