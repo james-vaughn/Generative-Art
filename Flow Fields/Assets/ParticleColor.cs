f2190bc5c5a7a81f9acfe7a820cbcb419da7f440
James Vaughn <james.thomas.vaughn@gmail.com>
2018-03-01 11:47:43 -0500

    Have more of a smokey effect

--- Flow Fields/Assets/ParticleColor.cs
+++ Flow Fields/Assets/ParticleColor.cs
@@ -25,7 +25,7 @@ public class ParticleColor
 	public ParticleColor Mutate() {
 		var color = (float)rand.NextDouble ();
 		var sign = (float)rand.NextDouble () > .5f ? -1f : 1f;
-		var amount = (float)rand.NextDouble() * .02f;
+		var amount = (float)rand.NextDouble() * .03f;
 
 		if (color < .33f) {
 			R += sign * amount;
